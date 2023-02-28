744db9d2ea11bd1b7814c481063eee9cf9609ae3
Samczyk <Piotreksamek07@gmail.com>
2023-02-28 13:39:16 +0100

    Add offer layout and add Fixtures.

---
+++ src/Controller/OfferController.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\Offer;
+use App\Repository\OfferRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OfferController extends AbstractController
+{
+    #[Route('/offers', name: 'app_offers')]
+    public function offers(OfferRepository $offerRepository): Response
+    {
+        $offers = $offerRepository->findAll();
+        return $this->render('offers.html.twig', [
+            'offers' => $offers
+        ]);
+    }
+
+    #[Route('/offers/{slug}', name: 'app_offer_show')]
+    public function offer(Offer $offer): Response
+    {
+        return $this->render('offer.html.twig',[
+            'offer' => $offer
+        ]);
+    }
+}
---
+++ src/Factory/OfferFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Factory;
+
+use App\Entity\Offer;
+use Zenstruck\Foundry\ModelFactory;
+
+class OfferFactory extends ModelFactory
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    protected function initialize()
+    {
+        return $this;
+    }
+
+    protected function getDefaults(): array
+    {
+        return [
+            'name' => self::faker()->realText(20),
+            'description' => self::faker()->realText(255),
+            'price' => rand(3800, 10000),
+            'dateTime' => self::faker()->dateTimeBetween('-5days', '-1 minute'),
+            'slug' => self::faker()->slug(),
+            'city' => self::faker()->city(),
+            'company' => self::faker()->company(),
+        ];
+    }
+
+    protected static function getClass(): string
+    {
+        return Offer::class;
+    }
+
+}
